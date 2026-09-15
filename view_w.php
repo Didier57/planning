85ae15ef108430da302ab37e15d3b1eeb0befb75
Didier57 <didier@macchi.fr>
2026-09-15 14:20:26 +0200

    Show week number in week-with-users view (view_w) corner

--- view_w.php
+++ view_w.php
@@ -109,7 +109,10 @@ for ( $j = 0; $j < $viewusercnt; $j += $USERS_PER_TABLE ) {
       translate ( 'Double-click on empty cell to add new entry' ) . '"';
   echo '>
       <tr>
-        <th class="empty">&nbsp;</th>';
+        <th class="empty">' . ( $DISPLAY_WEEKNUMBER == 'Y'
+    ? '<span style="color:var(--h2color);font-size:.75rem;">'
+      . translate ( 'Week' ) . ' ' . date ( 'W', $wkstart + 86400 ) . '</span>'
+    : '&nbsp;' ) . '</th>';
 
   // $j points to start of this table/row.
   // $k is counter starting at 0.
